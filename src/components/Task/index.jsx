62cf67b6bd3aebc58fb55159b86218082acfa1b1
civarry <cjcarito@gmail.com>
2023-05-25 23:06:48 +0800

    added animation scroll

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,12 +1,47 @@
+import React, { useEffect, useRef } from "react";
 import styles from "./task.module.css";
 import { TbTrash } from "react-icons/tb";
 import { FiMail } from "react-icons/fi";
-// import { BsFillCheckCircleFill } from "react-icons/bs";
+
 export function Task({ task, onDelete }) {
+  const cardContainerRef = useRef(null);
+  const previousScrollPositionRef = useRef(0);
+
+  useEffect(() => {
+    const cardContainer = cardContainerRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const scrollDirection =
+        scrollPosition > previousScrollPositionRef.current ? "down" : "up";
+
+      if (scrollDirection === "down") {
+        const cardContainerOffset = cardContainer.offsetTop;
+        if (scrollPosition + windowHeight > cardContainerOffset) {
+          cardContainer.classList.add(styles.animate);
+          cardContainer.classList.remove(styles.animateReverse);
+        } else {
+          cardContainer.classList.remove(styles.animate, styles.animateReverse);
+        }
+      }
+
+      previousScrollPositionRef.current = scrollPosition;
+    };
+
+    handleScroll(); // Apply animation immediately on mount
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className={styles.task}>
       {/* This is the new layout */}
-      <div className={styles.cardContainer}>
+      <div ref={cardContainerRef} className={styles.cardContainer}>
         <div className={styles.cardHeader}>
           <div className={styles.abcStyle}>
             <span>ABC</span>
